485b95748b67671849325f3793bb213bcd733d40
lama saeed <lama saeed>
2017-01-30 03:21:32 +0300

    Permissions index page design edited

--- resources/views/permissions/index.blade.php
+++ resources/views/permissions/index.blade.php
@@ -1,60 +1,101 @@
-@extends('layouts.app')
+@extends('layouts.admin_layout')
+@include('layouts.top_bar')
+@section('dashboard')
 
-@section('content')
-<div class="container">
-<h3>PERMISSIONS</h3>
-<a href="{{route('permissions.create')}}" class="btn btn-info">CREATE NEW PERMISSION</a>
-<hr>
-<div class="col-sm-12">
-  <h3>PERMISSIONS' LIST</h3>
-  @if(!empty($permissions))
-  <div class="table-responsive table-condensed">
-  <table class="table">
-  @foreach($permissions as $permission)
-    <tr>
-<!-- permission label -->
-      <td>{{$permission->label}}</td>
-<!-- END of permission label -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="block-header">
+                <h2>DASHBOARD</h2>
+            </div>
+            <!-- Basic Table -->
+            <div class="row clearfix">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="card">
+                        <div class="header">
+                            <h2>
+                                PERMISSIONS
+                                <small>Show - Create - Edit - Delete permissions</small>
+                            </h2>
+                            <a href="{{route('permissions.create')}}" class="btn btn-info">CREATE NEW PERMISSION</a>
+                            <ul class="header-dropdown m-r--5">
+                                <li class="dropdown">
+                                    <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" permission="button" aria-haspopup="true" aria-expanded="false">
+                                        <i class="material-icons">more_vert</i>
+                                    </a>
+                                    <ul class="dropdown-menu pull-right">
+                                        <li><a href="{{route('permissions.create')}}">Create New Permission</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="body table-responsive">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>PERMISSION NAME</th>
+                                        <th>PERMISSION LABEL</th>
+                                        <th>SHOW</th>
+                                        <th>EDIT</th>
+                                        <th>DELETE</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                @if(!empty($permissions))
+                                @foreach($permissions as $permission)
+                                    <tr>
+                                        <th scope="row">1</th>
+                                        <td>{{$permission->name}}</td>
+                                        <td>{{$permission->label}}</td>
+                                        <td>
+                                          <a href="{{route('permissions.show',$permission->id)}}"
+                                          class="btn btn-primary">
+                                           <span class="glyphicon glyphicon-eye-open"></span>
+                                         </a>
+                                        </td>
 
+                                    <!-- permission edit button  -->
 
-<!-- permission show button and adding permissions -->
-      <td>
-        <a href="{{route('permissions.show',$permission->id)}}"
-        class="btn btn-primary">
-         <span class="glyphicon glyphicon-eye-open"></span>
-       </a>
-      </td>
-<!-- END of permission show button and adding permissions -->
+                                            <td><a href="{{route('permissions.edit',$permission->id)}}"
+                                                   class="btn btn-warning ">
+                                              <span class="glyphicon glyphicon-edit"></span></a></td>
 
+                                          </td>
+                                    <!-- END permission edit button  -->
 
+                                    
 
-<!-- permissionpermission edit button  -->
+                                          
+                                    <!-- END permission edit button  -->
 
-        <td><a href="{{route('permissions.edit',$permission->id)}}"
-               class="btn btn-warning ">
-          <span class="glyphicon glyphicon-edit"></span></a></td>
-      <td>
-<!-- END permissionpermission edit button  -->
+                                        
 
+                                        <td>
+                                    <!-- permission Delete  button  -->
 
-<!-- permissionpermission Delete  button  -->
+                                         <form method="POST" action="{{route('permissions.destroy',$permission->id)}}">
+                                           {{method_field('DELETE')}}
+                                           {{ csrf_field() }}
+                                           <button type="submit"
+                                                    class="btn btn-danger ">
+                                           <span class="glyphicon glyphicon-trash"></span>
+                                         </button>
+                                         </form>
+                                          </td>
+                                    <!-- END permission Delete  button  -->
 
-     <form method="POST" action="{{route('permissions.destroy',$permission->id)}}">
-       {{method_field('DELETE')}}
-       {{ csrf_field() }}
-       <button type="submit"
-                class="btn btn-danger ">
-       <span class="glyphicon glyphicon-trash"></span>
-     </button>
-     </form>
-      </td>
-<!-- END permissionpermission Delete  button  -->
-    </tr>
-  @endforeach
-</table>
+                                       
+                                    </tr>
+                                @endforeach
+                                @endif
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- #END# Basic Table -->
 
-</div><!-- end permission list -->
-  @endif
-</div> <!--end of row class -->
 </div><!-- end of container -->
+</section>
 @endsection
